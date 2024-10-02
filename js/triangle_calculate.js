211efc12282c3a01d9bfcaf3e3c065d3f02ede5b
Mohammod-Sayed <mohammodsayed536@gmail.com>
2024-10-02 10:52:05 -0400

    first commit

--- js/triangle_calculate.js
+++ js/triangle_calculate.js
@@ -4,4 +4,9 @@ function calculateTriangleArea(){
         const triangleHeightInputValue = inputValueByID('triangle_height')
         const triangleArea = 0.5 * triangleBaseInputValue * triangleHeightInputValue
     })
+    const showButton = document.createElement('button')
+    const showText = document.createTextNode('Convert to m2')
+    showButton.appendChild(showText)
+    const showArea = document.getElementById('show_area')
+    showArea.appendChild(showButton)
 }
